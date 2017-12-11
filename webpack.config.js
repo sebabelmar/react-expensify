647776a792f226f6f895f57699492e7512c8a095
Seba Belmar <si.belmar@gmail.com>
2017-12-10 21:12:23 -0700

    Included babel and devtools on webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,15 @@ module.exports = {
   },
   devServer: {
     contentBase: path.join(__dirname, 'public')
-  }
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
 };
