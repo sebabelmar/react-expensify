13152ed0a7e1e02d237ced2e7ff0cdb2bd4f0eed
Seba Belmar <si.belmar@gmail.com>
2017-12-10 21:42:48 -0700

    Turned functional logic into components.

--- src/components/ListWidget.js
+++ src/components/ListWidget.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import getFeatures from '../api';
+import UnorderList from './UnorderList'
 
 export default class ListWidget extends React.Component{
 
@@ -12,35 +13,18 @@ export default class ListWidget extends React.Component{
       .then((features) => this.setState(() => ({ features })));
   }
   handgleToggle = (e, data) => {
-
     console.log(e, data)
-  }
-  createLI = (feature, index, hidden) => {
-    return(
-      <li
-        onClick={(e, data)=>{this.handgleToggle(e, data)}}
-        hidden={hidden}
-        key={Math.random()}
-      >
-        {feature.title}
-        {this.crateUL(feature.subfeatures, true)}
-      </li>
-    )
-  }
-  crateUL = (subfeatures, hidden) => {
-    if (!subfeatures || subfeatures.length === 0) {
-      return;
-    }
-    return (
-      <ul>
-        {subfeatures.map((feature, index) => this.createLI(feature, index, hidden))}
-      </ul>
-    )
   }
   render(){
     return (
       <div>
-        {this.crateUL(this.state.features, false)}
+        {
+          this.state.features.length > 0 &&
+          <UnorderList
+            array={this.state.features}
+            hidden={false}
+          />
+        }
       </div>
     );
   };
