3b739b798521edabf8f9d9dd5f955aed31e5d7b2
Seba Belmar <si.belmar@gmail.com>
2017-12-10 22:53:52 -0700

    Added SASS, basic styling and babel rule.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ListWidget from './components/ListWidget'
+import 'normalize.css/normalize.css'
+import './styles/style.scss'
 
 ReactDOM.render(<ListWidget />, document.getElementById('app'));
