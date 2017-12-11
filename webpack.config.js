54f9a966439c5bb1bf32d846f699936ac9e4f7b1
Seba Belmar <si.belmar@gmail.com>
2017-12-11 02:24:25 -0700

    Added configuration, rules for img and fonts.

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,31 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
         test: /\.s?css$/,
         use: [
           'style-loader',
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   },
