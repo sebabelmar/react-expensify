2b70345493393a935785ff0b844d1d2f7d2ab1f5
Seba Belmar <si.belmar@gmail.com>
2017-12-25 06:42:04 -0500

    Init + basic routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HipcampContainer from './components/HipcampContainer'
-import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
 
-ReactDOM.render(<HipcampContainer />, document.getElementById('app'));
+
+const AddExpensesPage = () => (
+  <div>
+    This is the add expenses page.
+  </div>
+);
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is the dashboard
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is the edit expense page
+  </div>
+)
+
+const HelpPage = () => (
+  <div>
+    This is the help page
+  </div>
+)
+
+const NotFoundPage = () => (
+  <div>
+    404! <Link to='/'>Go Home!</Link>
+  </div>
+)
+
+const Header = () => (
+  <div>
+    <h1>Expensify</h1>
+    <NavLink to='/' activeClassName='is-active' exact={true}>Home</NavLink>
+    <NavLink to='/create' activeClassName='is-active'>Add</NavLink>
+    <NavLink to='/edit' activeClassName='is-active'>Edit</NavLink>
+    <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
+  </div>
+)
+
+const router = (
+  <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path='/' component={ ExpenseDashboardPage } exact={true}/>
+        <Route path='/create' component={ AddExpensesPage } />
+        <Route path='/edit' component={ EditExpensePage } />
+        <Route path='/help' component={ HelpPage } />
+        <Route component={NotFoundPage}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(router, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
   },
   module: {
     rules: [
