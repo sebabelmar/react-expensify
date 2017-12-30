908478c573738d9817acc678dc7903ce990b8b9b
Seba Belmar <si.belmar@gmail.com>
2017-12-30 16:42:09 -0600

    Added desc, amount and note fields to edit form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,14 +1,47 @@
 import React from 'react';
 
 export default class ExpenseForm extends React.Component {
-
+  state = {
+    description: '',
+    note: '',
+    amount: ''
+  };
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  }
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  }
+  onAmountChange = (e) => {
+    e.persist();
+    const amount = e.target.value;
+    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount: e.target.value }));
+    }
+  }
   render() {
     return (
       <div>
         <form action="">
-          <input type="text"/>
-          <input type="number"/>
-          <textarea name="" id="" cols="30" rows="10"/>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input
+            type="number"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          />
+          <textarea
+            placeholder="Add a note"
+            value={this.state.note}
+            onChange={this.onNoteChange}
+          />
           <button>Subimt</button>
         </form>
       </div>
