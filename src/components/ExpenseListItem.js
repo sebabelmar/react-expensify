357d865cfc9652b854219b163c39337b5f3d5f2a
Seba Belmar <si.belmar@gmail.com>
2017-12-30 09:52:16 -0600

    Refactored remove implementation to use distructive object on list item and action

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { removeExpense } from '../actions/expenses';
 import { connect } from 'react-redux';
 
-const ExpenseListItem = (props) => {
-  const {id, description, amount, createdAt} = props;
-  return (
-    <div>
-      <h3>{description}</h3>
-      <p>${amount} - {createdAt}</p>
-      <button onClick={()=>{
-        props.dispatch(removeExpense(id));
-      }}>Remove</button>
-    </div>
-  );
-};
+const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
+  <div>
+    <h3>{description}</h3>
+    <p>${amount} - {createdAt}</p>
+    <button onClick={()=>{
+      dispatch(removeExpense({ id }));
+    }}>Remove</button>
+  </div>
+);
 
 export default connect()(ExpenseListItem);
