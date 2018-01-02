9ed7afdaeceb5b792fa115c7e3e45eb409821bf6
Seba Belmar <si.belmar@gmail.com>
2018-01-02 15:35:58 -0600

    Removed expenses samples from index.js

--- src/index.js
+++ src/index.js
@@ -3,35 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
-// REDUX examples....
 const store = configureStore();
-
-// addExpense
-store.dispatch(addExpense({
-  description: 'water bill',
-  amount: 5000,
-  createdAt: 1000
-}));
-
-store.dispatch(addExpense({
-  description: 'gas bill',
-  amount: 3000,
-  createdAt: 500
-}));
-
-store.dispatch(addExpense({
-  description: 'fuel bill',
-  amount: 1000,
-  createdAt: 250
-}));
-
-// REDUX examples....
 
 const jsx = (
   <Provider store={store} >
