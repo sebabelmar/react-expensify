387112322665315cba2d0ae76eac1a22f64ed5f9
Seba Belmar <si.belmar@gmail.com>
2018-01-02 06:25:53 -0600

    Added to json serializer to jest config

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,5 +5,5 @@ import Header from '../../components/Header';
 test('should render Header correclty', () => {
   const wrapper = shallow(<Header />);
 
-  expect(wrapper.find('h1').length).toBe(1);
+  expect(wrapper).toMatchSnapshot();
 });
